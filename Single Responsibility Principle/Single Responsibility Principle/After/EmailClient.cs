a6e8bf17dbbff1a13540d7811c1b51a268bd8040
mostafaelsayad745 <108666389+mostafaelsayad745@users.noreply.github.com>
2023-02-15 12:29:44 +0200

    Add files via upload

---
+++ Single Responsibility Principle/Single Responsibility Principle/After/EmailClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility_Principle.After
+{
+    internal class EmailClient
+    {
+        public void send (Account account, string transactionMessage , DateTime transactionDate)
+        {
+            // mock process for sending email
+            Console.WriteLine(
+             $"\n\n\t\t To: {account.Email}" +
+             $"\n\t\t Subject: Fake Bank Account Activity" +
+             $"\n\n\t\t Dear {account.Name}," +
+             $"\n\n\t\t\t A recent activity on your account occures at {transactionDate.ToString("yyyy-MM-dd hh:mm")}" +
+             "\n\t\t\t\t ===> {0}" +
+             $"\n\n\t\t Thank You,\n\t\t Fake Bank." +
+             $"\n\n\t\t--------------------------- ", transactionMessage);
+        }
+    }
+}
---
+++ Single Responsibility Principle/Single Responsibility Principle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace SOLID.SRP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestWithoutSRP(); 
+            TestWithSRP();
+            Console.ReadKey();
+        }
+
+
+        private static void TestWithoutSRP()
+        {
+            var account =
+                new Before.Account("Reem", "reem@example.com", 10000m);
+            account.MakeTransaction(500);
+            account.MakeTransaction(-11000);
+        }
+
+        private static void TestWithSRP()
+        {
+            var account =
+                new Single_Responsibility_Principle.After.Account("Reem", "reem@example.com", 10000m);
+
+            var accountService = new Single_Responsibility_Principle.After.AccountService();
+            accountService.Deposit(account, 500);
+            accountService.WithDraw(account, 11000);
+        }
+
+
+    }
+}
